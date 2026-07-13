c5f4a329b486392e0ae161aca1b44c0d0374547c
Lai Jun Yan <b10601043@g.ntu.edu.tw>
2026-07-13 14:29:06 +0800

    新增註解，讓自己看得懂

--- Aim_2/資料探勘/cell之間的網絡關係.R
+++ Aim_2/資料探勘/cell之間的網絡關係.R
@@ -15,11 +15,13 @@ cell_type <- expr$cell_type
 expr <- expr[, colnames(expr) != "cell_type"]
 
 # 根據WGCNA的資料格式進行轉至
+# row為 observation (gene)、col為想要分出module的變數
 datExpr <- t(expr)
 
-# 檢查資料品質
+# 使用多核心進行運算
 enableWGCNAThreads()
 
+# 檢查資料品質
 gsg <- goodSamplesGenes(
   datExpr,
   verbose = 3
@@ -55,7 +57,8 @@ text(
   col="red"
 )
 
-abline(h=0.8,col="red")
+abline(h=0.8,col="red") # 為了讓網絡符合生物學意義，選定R^2當作標準
+                        # 但其實如果是cell module，則不一定需要
 
 plot(
   sft$fitIndices[,1],
@@ -76,24 +79,29 @@ text(
 # 建立 adjacency matrix
 softPower <- 14
 
+# 算cell之間的關聯性
 adjacency <- adjacency(
   datExpr,
   power = softPower,
   type = "signed"
 )
 
+# 若靠慮到細胞之間有相連著許多共同的細胞，
+# 則會相應的調整細胞之間的相關性
 TOM <- TOMsimilarity(
   adjacency,
   TOMType = "signed"
 )
 
+# 將相關性轉換成距離，越相關則距離就越近
 dissTOM <- 1 - TOM
 
 cellTree <- hclust(
   as.dist(dissTOM),
-  method = "average"
+  method = "average" # cluster之間計算距離的方式
 )
 
+# 分出module
 dynamicMods <- cutreeDynamic(
   dendro = cellTree,
   distM = dissTOM,
@@ -104,6 +112,23 @@ dynamicMods <- cutreeDynamic(
 
 dynamicColors <- labels2colors(dynamicMods)
 
+# 不使用 cell type label 的情況下，gene expression 是否包含足夠資訊，
+# 讓 network clustering 自動恢復 cell identity？
+module_table <- table(
+  dynamicColors,
+  cell_type
+)
+
+# 視覺化不同cell type在各module中的分布情形
+# 評估module與cell type之間的對應關係
+# 用比例畫圖
+pheatmap(
+  prop.table(module_table, margin=2), # margin = 1表示module的組成
+  cluster_rows = FALSE,               # margin = 2表示cell type的分配
+  cluster_cols = TRUE
+)
+
+# 將分支圖與module放在一起看
 plotDendroAndColors(
   cellTree,
   dynamicColors,
@@ -112,37 +137,11 @@ plotDendroAndColors(
   hang = 0.03
 )
 
-# 判斷module與cell type是否一致
-module_table <- table(
-  dynamicColors,
-  cell_type
-)
-
-module_table
-
-###
+###計算ARI，量化結果
 module_df <- data.frame(
   cell = colnames(datExpr),
   module = dynamicColors,
   cell_type = cell_type
-)
-
-head(module_df)
-
-module_prop <- prop.table(
-  table(
-    module_df$module,
-    module_df$cell_type
-  ),
-  margin = 1
-)
-
-module_prop
-
-pheatmap(
-  module_prop,
-  cluster_rows = FALSE,
-  cluster_cols = TRUE
 )
 
 adjustedRandIndex(
